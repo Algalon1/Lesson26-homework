badb3651d61e52d508ea63b99984a521cd322f25
Algalon <kirill.bernatskyi@gmail.com>
2023-12-13 21:54:18 +0100

    described Car class

---
+++ src/Car.java
@@ -0,0 +1,8 @@
+public class Car extends Vehicle{
+    public String model;
+
+    public Car(String make, String model, String model1) {
+        super(make, model);
+        this.model = model1;
+    }
+}
