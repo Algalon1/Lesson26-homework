dce79901bd486f0f9d765399d2b466195ecc1f3f
Algalon <kirill.bernatskyi@gmail.com>
2023-12-13 21:57:29 +0100

    override toString method for Car class

--- src/Car.java
+++ src/Car.java
@@ -1,8 +1,14 @@
 public class Car extends Vehicle{
     public String model;
 
-    public Car(String make, String model, String model1) {
-        super(make, model);
-        this.model = model1;
+
+    public Car(String make, int year, String model) {
+        super(make, year);
+        this.model = model;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Make is %s, model is %s, year is %d",this.make,this.model,this.year);
     }
 }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,9 +1,9 @@
 public class Vehicle {
     public String make;
-    public String model;
+    public int year;
 
-    public Vehicle(String make, String model) {
+    public Vehicle(String make, int year) {
         this.make = make;
-        this.model = model;
+        this.year = year;
     }
 }
