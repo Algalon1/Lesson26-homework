7b3cc3514421740ea4082b9a874edee1df935415
Algalon <kirill.bernatskyi@gmail.com>
2023-12-13 21:53:23 +0100

    described Vehicle class

---
+++ src/Vehicle.java
@@ -0,0 +1,9 @@
+public class Vehicle {
+    public String make;
+    public String model;
+
+    public Vehicle(String make, String model) {
+        this.make = make;
+        this.model = model;
+    }
+}
